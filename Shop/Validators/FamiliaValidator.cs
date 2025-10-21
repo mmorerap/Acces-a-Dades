41259708c617cc0fee0e2d2d02d917ab63aadd1e
mmorerap <marcmoreraprat@gmail.com>
2025-10-21 17:12:34 +0200

    Creacio nova validacio de la familia

--- Shop/Validators/FamiliaValidator.cs
+++ Shop/Validators/FamiliaValidator.cs
@@ -9,11 +9,18 @@ public static class FamiliaValidator
 {
     public static Result Validate(FamiliaRequest familia)
     {
-        if (familia.Nom is null )
+        if (familia.Nom != "")
+        {
+            return Result.Failure("No ha posat cap Nom a la familia", "NOM_EMPTY");
+        }
+
+        if (familia.Nom.Length > 20 )
         {
             return Result.Failure("No ha posat cap Nom a la familia","NOM_EMPTY");
         }
         return Result.Ok();
     }
+    
+    
 
 }
